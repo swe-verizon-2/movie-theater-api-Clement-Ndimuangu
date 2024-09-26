b872e0a3c8ad159af6f2ad2855187f2f73965c35
Clement Ndimuangu <clement.ndimuangu@verizon.com>
2024-09-26 14:29:28 -0500

    done with the project

--- routes/shows.js
+++ routes/shows.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const {Show} = require('../models/index')
+const {check, validationResult} = require('express-validator')
 
 router.get('/', async(req,res)=>{
     const genre = req.query.genre;
@@ -19,19 +20,24 @@ router.get('/:id', async(req, res)=>{
     res.json(show)
 })
 
-router.get('/users/:id', async(req, res)=>{
+router.get('/:id/users', async(req, res)=>{
     const id = req.params.id
     const show = await Show.findByPk(id)
     const usersWatchShow = await show.getUsers()
     res.json(usersWatchShow)
 })
 
-router.put('/:id', async(req, res)=>{
-    const body = req.body
-    const id = req.params.id
-    const showToUpdate = await Show.findByPk(id)
-    const updatedShow = await showToUpdate.update(body)
-    res.json(updatedShow)
+router.put('/:id/available', [check('title').isLength({max:20})], async(req, res)=>{
+    const errors = validationResult(req)
+    if(!errors.isEmpty()){
+        res.json({error: errors.array()})
+    }else{
+        const body = req.body
+        const id = req.params.id
+        const showToUpdate = await Show.findByPk(id)
+        const updatedShow = await showToUpdate.update(body)
+        res.json(updatedShow)
+    }
 })
 
 router.delete('/:id', async(req,res)=>{
--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const router = express.Router()
 const { User, Show} = require('../models/index')
+const {check, validationResult, body} = require('express-validator')
 
 router.get("/", async(req,res)=>{
     const users = await User.findAll({ include: { all: true, nested: true } })
@@ -13,19 +14,31 @@ router.get("/:id", async(req,res)=>{
     res.json(user)
 })
 
-router.get("/shows/:id", async(req, res)=>{
+router.get("/:id/shows", async(req, res)=>{
     const id = req.params.id
     const user = await User.findByPk(id)
     const shows = await user.getShows()
     res.json(shows)
 })
 
-router.put("/shows/:id", async(req, res)=>{
-    const body = req.body;
-    const id = req.params.id;
-    const user = await User.findByPk(id)
-    await user.addShows(body)
-    res.json(body)
+router.put("/:userId/shows/:showId",async(req, res)=>{
+    const userId = req.params.userId;
+    const showId = req.params.showId;
+    const user = await User.findByPk(userId)
+    const show = await Show.findByPk(showId)
+    await user.addShows(show)
+    res.json(show)  
+})
+
+router.post('/', [check('username').isEmail()],async(req, res)=>{
+    const errors = validationResult(req)
+    const body = req.body
+    if(!errors.isEmpty()){
+        res.json({error: errors.array()})
+    }else{
+        const newUser = await User.create(body)
+        res.json(newUser)
+    }
 })
 
 
